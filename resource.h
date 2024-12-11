004a9e7ebd832a3a954f0487c4654593a12eba4c
stx <80523829+stx3plus1@users.noreply.github.com>
2024-12-11 22:51:40 +0000

    add them to resource.h

--- resource.h
+++ resource.h
@@ -1,3 +1,17 @@
-#define IDB_WELCOME                     129
-#define IDD_WELCOMEBOX                  130
-#define IDC_STATIC                      -1
+/*
+ * Control element IDs
+ */
+#define IDB_WELCOME 129
+#define IDD_WELCOMEBOX 130
+#define IDC_STATIC -1
+#define IDC_NEXTTIP 16901
+#define IDC_DIDYOUKNOW 16910
+#define IDC_TEXTCONTROL 16908
+#define IDC_TIP 16905
+#define IDC_WHATSNEW 16898
+#define IDC_ONLINEREGISTRATION 16899
+#define IDC_WINDOWSTOUR 16897
+#define IDC_CATALOG 16900
+#define IDC_APPICON 16909
+#define IDC_BITMAP 16906
+#define IDC_CHECKBOX 16903
