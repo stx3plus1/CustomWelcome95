08faf9eb96fdf426bcb986babfa4672648727d18
stx3plus1 <stx4@ballpa.in>
2024-12-11 22:20:29 +0000

    version 2.0

--- CustomWelcome95.cpp
+++ CustomWelcome95.cpp
@@ -1,134 +1,148 @@
+/*
+ * CustomWelcome95
+ * by stx3plus1
+ */
+
 #include <windows.h>
-#include <cstdlib>
-#include <ctime>
 #include <string>
+#include <ctime>
+
 #include "resource.h"
+
+/*
+ * Control element IDs
+ */
 
 #define IDC_NEXTTIP 16901
 #define IDC_DIDYOUKNOW 16910
 #define IDC_TEXTCONTROL 16908
-#define IDC_STATICTEXT 16905
+#define IDC_TIP 16905
 #define IDC_WHATSNEW 16898
 #define IDC_ONLINEREGISTRATION 16899
-#define IDC_ICON 16909
+#define IDC_WINDOWSTOUR 16897
+#define IDC_CATALOG 16900
+#define IDC_APPICON 16909
+#define IDC_BITMAP 16906
+#define IDC_CHECKBOX 16903
 
-// #define funny
+/*
+ * Various strings
+ */
 
 const char* strings[] = {
-#ifdef funny
-    "im playing minecraft...",
-#endif
-    "WHATEVER BLOOD IS LEFT INSIDE YOU IS IN ME NOW TOO",
-    "To run Windows in safe mode, lock your doors and take Mother out of storage.",
-    "On December 31st, 2001, Microsoft will end support for Windows 95.\n\nBut Windows 95 will be strong enough to support itself.",
-    "Your Internet connection is working fine;\nNo one replies to your e-mails because there is no one left.",
-    /* Needs patching
-    "If you listen to CLOUDS.MID backwards, you will go to sleep and have an amazing dream. In it, one by one, you will meet the Microsoft Windows 95 Product Team.\nFinally, you get to Mr. Gates. You reach out to shake his hand, but instead, he puts some-thing in your palm. He smiles. You wake up.\n\nIn your hand, will be a small, white egg.",
-    */
-    "A small needle at the base of your mouse has been gradually collecting your blood.\n\nThe backup is almost complete.",
-    "You now smell something not unlike rotting meat every time you boot me up.\nI cannot promise that the smell will ever go away, or that you will find the source.\n\nBut I can promise that you will learn to love it.",
-    "In the last six months, 14 people from your town have been reported missing.\n\nYou haven't checked your Recycle Bin in about that long.",
-    "You can use Windows Explorer to explore ideas too disturbing for normal humans to fathom.\nIf you start to go mad, run ScanDisk.",
-    "You can use Find to locate your real Father.\n\nBut I will save you the trouble.\n\nWindows 95 is your real father.",
-    "If you open your computer, inside you will find a cloth doll, soft and worn.\nYou will suddenly remember owning this doll as a child, loving and cherishing it.\n\nDo not question the truth of this memory.",
     "Loneliness is an emotion.\nEmotions are weakness.\n\nUninstall them by going to the Control Panel and selecting Add/Remove Emotions.",
-    "Double click the Network Neighbourhood icon on your desktop to access the computers of everyone you've ever known that died.",
-    "There are function keys way beyond F12 that you are not ready for.\nMost of them would burn your fucking finger off.",
-    "Destroy all documents",
-    "Touch your monitor. It is warm, like flesh.\nBut it is not flesh.\nNot yet."
+	"I need MS-DOS. Please give me MS-DOS.",
+	"You have no importance here\nYou can now leave\nYou are allowed to exit",
+	"You didn't know this.",
+	"Notice: If you notice this notice you will notice that this notice is not worth noticing.",
+	"Destroy System32",
+	"where's the fucking soil",
+	"the cake is a lie",
+	"The longer the icon of sin stays on earth, the stronger he will become"
 };
-
 const int numStrings = sizeof(strings) / sizeof(strings[0]);
 
-const char* errmsg = "There is nowhere you can run.";
-const char* warnmsg = "You keep checking the date. You must be confused. It's 1995.\nThat feeling that you installed Windows 95 a long time ago is just confusion.\nWindows 95 will now run Solitaire to calm you.";
+const char* msgtitle = "Microsoft Windows";
+const char* errmsg = "Nothing's new. Windows 95 has been installed for quite a while.";
+const char* warnmsg = "Windows 95 does not think so.\nYou have been revoked of your network access.\n\"Try again later\".";
+const char* tourmsg = "The Windows 95 tour has been missing for a long time.";
+const char* catalog = "You already installed it all.";
+const char* tipwarning = "You weren't supposed to do that.";
 
-int index;
+/* 
+ * Code
+ */
+
+int strIndex;
 HBRUSH hBackgroundBrush;
 
-void SetBoldFont(HWND hDlg, HWND hStaticText) {
-    HFONT hFont, hOldFont;
-    LOGFONT lf = { 0 };
-    hFont = (HFONT)SendMessage(hStaticText, WM_GETFONT, 0, 0);
-    if (hFont) {
-        GetObject(hFont, sizeof(LOGFONT), &lf);
-    }
-    lf.lfWeight = FW_BOLD;
-    hFont = CreateFontIndirect(&lf);
-    hOldFont = (HFONT)SendMessage(hStaticText, WM_SETFONT, (WPARAM)hFont, TRUE);
-    if (hOldFont) {
-        DeleteObject(hOldFont);
-    }
-}
-
-INT_PTR CALLBACK MainDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
-    static HWND hStaticText, hTextControl, hDidYouKnow, hIcon;
-
+BOOL CALLBACK MainDialogProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
-    case WM_INITDIALOG:
-        srand(time(0));
-        index = rand() % numStrings;
-        SetDlgItemTextA(hDlg, IDC_STATICTEXT, strings[index]);
-        hDidYouKnow = GetDlgItem(hDlg, IDC_DIDYOUKNOW);
-        hStaticText = GetDlgItem(hDlg, IDC_STATICTEXT);
-        hTextControl = GetDlgItem(hDlg, IDC_TEXTCONTROL);
-        hIcon = GetDlgItem(hDlg, IDC_ICON);
-        SetBoldFont(hDlg, hDidYouKnow);
-        hBackgroundBrush = CreateSolidBrush(RGB(255, 255, 225));
-        SendDlgItemMessage(hDlg, 16903, BM_SETCHECK, BST_CHECKED, 0);
-        return (INT_PTR)TRUE;
-    case WM_CTLCOLORSTATIC:
-        if ((HWND)lParam == hTextControl || (HWND)lParam == hStaticText || (HWND)lParam == hDidYouKnow || (HWND)lParam == hIcon) {
-            SetBkColor((HDC)wParam, RGB(255, 255, 225)); 
-            return (INT_PTR)hBackgroundBrush;
-        }
-        break;
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        if (LOWORD(wParam) == IDC_NEXTTIP) {
-            index++;
-            if ((index + 1) > numStrings) {
-                index = 0;
-            }
-            SetDlgItemTextA(hDlg, IDC_STATICTEXT, strings[index]);
-            return (INT_PTR)TRUE;
-        }
-        if (LOWORD(wParam) == IDC_WHATSNEW) {
-            while (1) {
-                MessageBoxA(hDlg, errmsg, "Warning", MB_ICONSTOP);
-            }
-        }
-        if (LOWORD(wParam) == IDC_ONLINEREGISTRATION) {
-            MessageBoxA(hDlg, warnmsg, "Windows 95", MB_ICONWARNING);
-        }
-        if (LOWORD(wParam) == 16903) {
-            wchar_t randomChar;
-            std::wstring randomString;
-            while (1) {
-                for (int i = 0; i < 10; i++) {
-                    randomChar = rand() % 0xFFFF;
-                    randomString += randomChar;
-                }
-                int bufferSize = WideCharToMultiByte(CP_UTF8, 0, randomString.c_str(), -1, NULL, 0, NULL, NULL);
-                std::string narrowStr(bufferSize, 0);
-                WideCharToMultiByte(CP_UTF8, 0, randomString.c_str(), -1, &narrowStr[0], bufferSize, NULL, NULL);
-                LPCSTR lpcstr = narrowStr.c_str();
-                SetDlgItemTextA(hDlg, IDC_STATICTEXT, lpcstr);
-            }
-        }
-        break;
+		case WM_INITDIALOG: {
+			srand(time(NULL));
+			strIndex = rand() % numStrings;
 
-    case WM_CLOSE:
-        EndDialog(hDlg, 0);
-        return (INT_PTR)TRUE;
+			SetDlgItemTextA(hDlg, IDC_TIP, strings[strIndex]);
+
+			HWND hDidYouKnow = GetDlgItem(hDlg, IDC_DIDYOUKNOW);
+			HWND hTip = GetDlgItem(hDlg, IDC_TIP);
+			HWND hTextControl = GetDlgItem(hDlg, IDC_TEXTCONTROL);
+			HWND hIcon = GetDlgItem(hDlg, IDC_APPICON);
+
+			HFONT hFont, hOldFont;
+			LOGFONT lf = { 0 };
+			hFont = (HFONT)SendMessage(hTip, WM_GETFONT, 0, 0);
+			if (hFont) GetObject(hFont, sizeof(LOGFONT), &lf);
+			lf.lfWeight = FW_BOLD;
+			hFont = CreateFontIndirect(&lf);
+			hOldFont = (HFONT)SendMessage(hDidYouKnow, WM_SETFONT, (WPARAM)hFont, TRUE);
+			if (hOldFont) DeleteObject(hOldFont);
+
+			hBackgroundBrush = CreateSolidBrush(RGB(255, 255, 225));
+			SendDlgItemMessage(hDlg, 16903, BM_SETCHECK, BST_CHECKED, 0);
+			
+			HBITMAP hBitmap = (HBITMAP)LoadImage(GetModuleHandle(NULL), MAKEINTRESOURCE(IDB_WELCOME), IMAGE_BITMAP, 0, 0, LR_LOADMAP3DCOLORS);
+			SendDlgItemMessage(hDlg, IDC_BITMAP, STM_SETIMAGE, 0, (LPARAM)hBitmap);
+		}
+			return (INT_PTR)TRUE;
+		case WM_CTLCOLORSTATIC: {
+			int id = GetWindowLong((HWND)lParam, GWL_ID);
+				if (id != IDC_CHECKBOX) {
+					SetBkMode((HDC)wParam, TRANSPARENT); 
+					return (INT_PTR)hBackgroundBrush;
+				}
+			}
+			break;
+		case WM_COMMAND:
+			if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL) {
+				EndDialog(hDlg, LOWORD(wParam));
+				return (INT_PTR)TRUE;
+			}
+			if (LOWORD(wParam) == IDC_NEXTTIP) {
+				HWND hCheckbox = GetDlgItem(hDlg, IDC_CHECKBOX);
+				int state = SendMessage(hCheckbox, BM_GETCHECK, 0, 0);
+				if (state == BST_CHECKED) {
+					strIndex++;
+					if ((strIndex + 1) > numStrings) {
+						strIndex = 0;
+					}
+					SetDlgItemTextA(hDlg, IDC_TIP, strings[strIndex]);
+				} else
+					SetDlgItemTextA(hDlg, IDC_TIP, tipwarning);
+				return (INT_PTR)TRUE;
+			}
+			if (LOWORD(wParam) == IDC_WHATSNEW) {
+				MessageBoxA(hDlg, errmsg, msgtitle, MB_ICONWARNING);
+				return (INT_PTR)TRUE;
+			}
+			if (LOWORD(wParam) == IDC_ONLINEREGISTRATION) {
+				MessageBoxA(hDlg, warnmsg, msgtitle, MB_ICONSTOP);
+				return (INT_PTR)TRUE;
+			}
+			if (LOWORD(wParam) == IDC_WINDOWSTOUR) {
+				MessageBoxA(hDlg, tourmsg, msgtitle, MB_ICONWARNING);
+			}
+			if(LOWORD(wParam) == IDC_CATALOG) {
+				MessageBoxA(hDlg, catalog, msgtitle, MB_ICONSTOP);
+			}
+			if (LOWORD(wParam) == IDC_CHECKBOX) {
+				HWND hCheckbox = GetDlgItem(hDlg, IDC_CHECKBOX);
+				int state = SendMessage(hCheckbox, BM_GETCHECK, 0, 0);
+				if (state == BST_CHECKED)
+					SetDlgItemTextA(hDlg, IDC_TIP, strings[strIndex]);
+				else
+					SetDlgItemTextA(hDlg, IDC_TIP, tipwarning);
+				return (INT_PTR)TRUE;
+			}
+			break;
+		case WM_CLOSE:
+			EndDialog(hDlg, 0);
+			return (INT_PTR)TRUE;
     }
     return (INT_PTR)FALSE;
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
-    DialogBox(hInstance, MAKEINTRESOURCE(IDD_WELCOMEBOX), NULL, MainDialogProc);
-    return 0;
+	DialogBox(hInstance, MAKEINTRESOURCE(IDD_WELCOMEBOX), NULL, MainDialogProc);
+	return 0;
 }
--- resource.h
+++ resource.h
@@ -1,7 +1,3 @@
-//{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by CustomWelcome95.rc
-//
 #define IDC_MYICON                      2
 #define IDD_CUSTOMWELCOME95_DIALOG      102
 #define IDS_APP_TITLE                   103
@@ -16,15 +12,3 @@
 #define IDD_WELCOMEBOX                  130
 #define IDC_HIDDENBUTTON                1000
 #define IDC_STATIC                      -1
-
-// Next default values for new objects
-// 
-#ifdef APSTUDIO_INVOKED
-#ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
-#define _APS_NEXT_SYMED_VALUE           110
-#endif
-#endif
